fc875272d8194d727d3c0c9acd1569af1035d48d
michaelnASC6 <mnewby0503@gmail.com>
2019-07-29 12:02:24 -0400

    Start w Alessander

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,28 @@
+
+const x = document.getElementsByClassName("right_side");
+// const index0 = x[0];
+// const index0style = index0.style;
+// index0style.backgroundcolor = "-6px 5px 2px #FFFF00";
+
+ console.log(x)
+
+
+
+
+
+
+
+// document.getElementByClassName("name")[0].style.textShadow = "5px 5px 10px #00FF00";
+
+
+
+
+const classes = document.getElementsByClassName("name");
+const index0 = classes[0];
+const index0style = index0.style;
+index0style.textShadow = "-6px 5px 2px #FFFF00";
+
+
+const index1 = classes[1];
+const index1style = index1.style;
+index1style.textshadow = "-6px 5px 2px #FFFF00";
